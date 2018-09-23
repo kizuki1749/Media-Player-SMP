ddf17120ed57ada313527e42a878ecd913702753
暇人村長きずき / kizuki1749 <kizuki1749@yahoo.co.jp>
2018-09-23 10:12:33 +0900

    1.29 Release Candidate 2

--- Form1.cs
+++ Form1.cs
@@ -44,7 +44,7 @@ namespace Media_Player_SMP
         private const UInt32 MF_STRING = 0x00000000;
         private const UInt32 MF_SEPARATOR = 0x00000800;
         private const int WM_SYSCOMMAND = 0x112;
-        public string version = "1.29 Release Candidate 1";
+        public string version = "1.29 Release Candidate 2";
 
         public Form1()
         {
