7c934d51418103386879dec81f981d83e4afd7cc
暇人村長きずき / kizuki1749 <kizuki1749@yahoo.co.jp>
2018-10-10 21:48:40 +0900

    1.30 Alpha 1

--- Form1.cs
+++ Form1.cs
@@ -50,7 +50,7 @@ namespace Media_Player_SMP
         private const UInt32 MF_SEPARATOR = 0x00000800;
         private const int WM_SYSCOMMAND = 0x112;
         public DiscordRpcClient DiscordRpcClient = new DiscordRpcClient("495186532903157760", true);
-        public string version = "1.30 Pre-Alpha 5";
+        public string version = "1.30 Alpha 1";
 
         public Form1()
         {
